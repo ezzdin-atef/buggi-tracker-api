09f697313212b0658e9ad102668503d610bda1d1
Ezzdin Atef <ezzdin1125@gmail.com>
2020-08-18 20:23:35 +0200

    fix the port number

--- src/index.js
+++ src/index.js
@@ -30,4 +30,4 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(3000, () => console.log("Server Started"));
+app.listen(process.env.PORT || 3000, () => console.log("Server Started"));
